d1b982aa210e55893145bb6822f2017ee5dc47d9
victoriakhyzhniak <hystericalbird@gmail.com>
2024-11-11 15:14:13 +0200

    js added

---
+++ counter.js
@@ -0,0 +1,17 @@
+let counter = 0;
+
+            function count() {
+                counter ++;
+                document.querySelector("h1").innerHTML = counter;
+
+                if (counter % 10 === 0) {
+                    alert(`Counter is now ${counter}`);
+                }
+                //alert(counter) (сповіщення на сторінці, вспливаюче вікно);
+                
+            }
+
+            //додати слухача подій DOM - обʼєктна модель документа, структура сторінки):
+            document.addEventListener("DOMContentLoaded", function () {
+                document.querySelector ("button").addEventListener ("click", count);
+            })
